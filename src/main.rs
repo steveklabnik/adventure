6c1ea6ed80fe1ab3ad37c7187c9cab64d15488a7
Steve Klabnik <steve@steveklabnik.com>
2014-09-06 04:56:16 -0400

    Very simple first room.
    This is the absolute minimum to handle a single room.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
+#[deriving(PartialEq)]
+enum Command {
+    South,
+}
+
 fn main() {
-    println!("Hello, world!")
+    println!("* * * A D V E N T U R E * * *\n\n");
+
+    let mut command: Option<Command> = None;
+    let mut stdin = io::stdin();
+
+    while command == None {
+        println!("You find yourself in a room. There is a door to the south.");
+        println!("\nWhat do you do?\n");
+        println!("* Go (s)outh");
+
+        let input = stdin.read_line().ok().expect("Failed to read line");
+
+        command = match input.as_slice().trim() {
+            "s" => Some(South),
+            _   => {
+                println!("Please type a command.");
+                continue;
+            }
+        };
+    }
+
 }

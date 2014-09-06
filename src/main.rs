2f37a92d5a1ca36789f42739643f11179ad50be9
Steve Klabnik <steve@steveklabnik.com>
2014-09-06 09:43:24 -0400

    Process both rooms with the new function...
    ... and put them in a loop.
    
    Whoah, this is like almost a working game!

--- src/main.rs
+++ src/main.rs
@@ -64,32 +64,10 @@ fn main() {
     let mut command: Option<Command> = None;
     let mut stdin = io::stdin();
 
-    current_room = enter(&rooms[current_room]);
-
-    while command == None {
-        println!("You find yourself in a room. There is a door to the south, and a door to the north.");
-        println!("\nWhat do you do?\n");
-        println!("* Go (n)orth");
-        println!("* Go (s)outh");
-
-        let input = stdin.read_line().ok().expect("Failed to read line");
-
-        command = match input.as_slice().trim() {
-            "s" => Some(Go(South)),
-            "n" => Some(Go(North)),
-            _   => {
-                println!("Please type a command.");
-                continue;
-            }
-        };
+    while current_room != 2 { // hard code the exit for now...
+        current_room = enter(&rooms[current_room]);
     }
 
-    match command.unwrap() {
-        Go(North) => println!("Not implemented"),
-        Go(South) => println!("Not implemented"),
-    }
-
-    command = None;
 }
 
 fn enter(room: &Room) -> uint {

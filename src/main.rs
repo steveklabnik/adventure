f90f2824bcb5227eb044ca7fdd53ee27be824d2f
Steve Klabnik <steve@steveklabnik.com>
2014-09-06 05:03:19 -0400

    Second room, with some intermediate work.
    Because we don't have a game loop yet, we can't actually
    implement the logic in the correct way. The only thing that's
    really right here is that we validate input.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 
 #[deriving(PartialEq)]
 enum Command {
+    North,
     South,
 }
 
@@ -27,4 +28,35 @@ fn main() {
         };
     }
 
+    match command.unwrap() {
+        South => println!("Not implemented"),
+        North => println!("Not implemented"),
+    }
+
+    command = None;
+
+    while command == None {
+        println!("You find yourself in a room. There is a door to the south, and a door to the north.");
+        println!("\nWhat do you do?\n");
+        println!("* Go (n)orth");
+        println!("* Go (s)outh");
+
+        let input = stdin.read_line().ok().expect("Failed to read line");
+
+        command = match input.as_slice().trim() {
+            "s" => Some(South),
+            "n" => Some(North),
+            _   => {
+                println!("Please type a command.");
+                continue;
+            }
+        };
+    }
+
+    match command.unwrap() {
+        North => println!("Not implemented"),
+        South => println!("Not implemented"),
+    }
+
+    command = None;
 }

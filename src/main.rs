e1c5f13a59256fef60b4568816e64d1128930b7e
Steve Klabnik <steve@steveklabnik.com>
2014-09-06 09:21:00 -0400

    Start to extract an 'enter' function
    This function will be able to handle any room we give it. We're just
    using it for the first room right now.

--- src/main.rs
+++ src/main.rs
@@ -44,33 +44,14 @@ fn main() {
         },
     ];
 
+    let mut current_room = 0;
+
     println!("* * * A D V E N T U R E * * *\n\n");
 
     let mut command: Option<Command> = None;
     let mut stdin = io::stdin();
 
-    while command == None {
-        println!("You find yourself in a room. There is a door to the south.");
-        println!("\nWhat do you do?\n");
-        println!("* Go (s)outh");
-
-        let input = stdin.read_line().ok().expect("Failed to read line");
-
-        command = match input.as_slice().trim() {
-            "s" => Some(Go(South)),
-            _   => {
-                println!("Please type a command.");
-                continue;
-            }
-        };
-    }
-
-    match command.unwrap() {
-        Go(North) => println!("Not implemented"),
-        Go(South) => println!("Not implemented"),
-    }
-
-    command = None;
+    let next_room = enter(&rooms[current_room]);
 
     while command == None {
         println!("You find yourself in a room. There is a door to the south, and a door to the north.");
@@ -96,4 +77,33 @@ fn main() {
     }
 
     command = None;
+}
+
+fn enter(room: &Room) -> uint {
+    let mut command: Option<Command> = None;
+
+    while command == None {
+        println!("You find yourself in a room. There is a door to the south.");
+        println!("\nWhat do you do?\n");
+        println!("* Go (s)outh");
+
+        let input = io::stdin().read_line().ok().expect("Failed to read line");
+
+        command = match input.as_slice().trim() {
+            "s" => Some(Go(South)),
+            _   => {
+                println!("Please type a command.");
+                continue;
+            }
+        };
+    }
+
+    match command.unwrap() {
+        Go(North) => println!("Not implemented"),
+        Go(South) => println!("Not implemented"),
+    }
+
+    command = None;
+
+    1 // lol hax
 }

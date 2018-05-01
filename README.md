e141740a1246650dbaf2e7fa95d1809235fe67a4
Daniel W. Zhang <daniel.danielnew99@gmail.com>
2018-05-01 19:30:09 -0400

    Updates for compilation on modern Rust
    Transitions from std::old_io to std::io
    Changes let bindings to use for enums in pattern matching
    Replaces calls to syntax that is no longer used
    Changes README.md to reflect current working status

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 A simple text adventure game, in Rust.
 
-⚠️**Warning** ⚠️ This project was created before Rust 1.0 was stabilized and does not currently compile with any known stable Rust.
-
 [![Build Status](https://travis-ci.org/steveklabnik/adventure.svg)](https://travis-ci.org/steveklabnik/adventure)
 
 I used to write a text adventure in every new language I learned. Might as well
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::old_io;
+use std::io;
 
 #[derive(PartialEq)]
 enum Command {
@@ -21,8 +21,8 @@ struct Exit {
 }
 
 impl Exit {
-    fn can_go(&self, direction: Direction) -> bool {
-        self.direction == direction &&
+    fn can_go(&self, direction: &Direction) -> bool {
+        self.direction == *direction &&
         !self.locked
     }
 }
@@ -39,7 +39,7 @@ struct Item {
 
 impl Room {
     fn unlock(&mut self, direction: Direction) -> Option<usize> {
-        let exit = self.exits.mut_iter()
+        let exit = self.exits.iter_mut()
                              .find(|e| e.direction == direction)
                              .unwrap();
 
@@ -49,7 +49,7 @@ impl Room {
     }
 
     fn can_go(&self, direction: Direction) -> bool {
-        self.exits.iter().find(|e| e.can_go(direction)).is_some()
+        self.exits.iter().find(|e| e.can_go(&direction)).is_some()
     }
 
     fn exit_to(&self, direction: Direction) -> Option<usize> {
@@ -71,10 +71,10 @@ impl Room {
 
 fn main() {
 
-    let south = Direction::South;
-    let east = Direction::East;
-    let west = Direction::West;
-    let north = Direction::North;
+    use Direction::South as south;
+    use Direction::East as east;
+    use Direction::West as west;
+    use Direction::North as north;
 
     let mut rooms = vec![
         Room {
@@ -152,7 +152,7 @@ fn main() {
     println!("* * * A D V E N T U R E * * *\n\n");
 
     while !rooms[current_room].is_escape() {
-        current_room = enter(rooms.get_mut(current_room)).unwrap_or(current_room);
+        current_room = enter(rooms.get_mut(current_room).unwrap()).unwrap_or(current_room);
     }
 
     println!("Congrats! You've escaped.");
@@ -160,10 +160,10 @@ fn main() {
 
 fn enter(room: &mut Room) -> Option<usize> {
 
-    let south = Direction::South;
-    let east = Direction::East;
-    let west = Direction::West;
-    let north = Direction::North;
+    use Direction::South as south;
+    use Direction::East as east;
+    use Direction::West as west;
+    use Direction::North as north;
 
     let go = Command::Go;
     let unlock = Command::Unlock;
@@ -192,12 +192,14 @@ fn enter(room: &mut Room) -> Option<usize> {
             }
         }
 
-        let input = old_io::stdin()
-                        .read_line()
-                        .ok()
-                        .expect("Failed to read line");
+        let mut input = String::new();
+        
+        io::stdin()
+            .read_line(&mut input)
+            .ok()
+            .expect("Failed to read line");
 
-        command = match input.as_slice().trim() {
+        command = match input.trim() {
             "n" if room.can_go(north) => Some(go(north)),
             "e" if room.can_go(east)  => Some(go(east)),
             "s" if room.can_go(south) => Some(go(south)),
